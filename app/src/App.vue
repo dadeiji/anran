92671f6319f81e1b563b197bbdd8c2675ef0e9a8
dadeiji <mxt1367379>
2025-02-14 13:58:07 +0800

    完整版本

--- app/src/App.vue
+++ app/src/App.vue
@@ -23,13 +23,32 @@ let isPc = (function() {
     }
     return flag;
 }());
-let getDateDiff = function(startDate, endDate) {
-    var startTime = new Date(Date.parse(startDate.replace(/-/g, "/"))).getTime();
-    var endTime = new Date(Date.parse(endDate.replace(/-/g, "/"))).getTime();
-    var dates = Math.abs((startTime - endTime)) / (1000 * 60 * 60 * 24);
-    return dates;
-}
-document.title += getDateDiff((new Date()).getFullYear() + '-' + ((new Date()).getMonth() + 1) + '-' + (new Date()).getDate(), '2016-09-15') + 1 + '天';
+let getSecondsDiff = (startDate) => {
+  const start = new Date(startDate.replace(/-/g, '/'));
+  const now = new Date();
+  const diff = now.getTime() - start.getTime();
+  const totalSeconds = Math.floor(diff / 1000);
+
+  const days = Math.floor(totalSeconds / (24 * 60 * 60));
+  const hours = Math.floor((totalSeconds % (24 * 60 * 60)) / (60 * 60));
+  const minutes = Math.floor((totalSeconds % (60 * 60)) / 60);
+  const seconds = totalSeconds % 60;
+
+  return {
+    days: days,
+    hours: hours,
+    minutes: minutes,
+    seconds: seconds,
+    totalDays: Math.floor(totalSeconds / (24 * 60 * 60)),
+    totalHours: Math.floor(totalSeconds / (60 * 60)),
+    totalMinutes: Math.floor(totalSeconds / 60),
+    totalSeconds: totalSeconds
+  };
+};
+
+const startDate = '2025-01-31 23:07:00';
+const secondsDiff = getSecondsDiff(startDate);
+document.title += secondsDiff.totalSeconds + '秒';
 export default {
     name: 'app',
     components: {
@@ -43,11 +62,12 @@ export default {
             enableHtml: false,
             fullStyle: [
                 `/*
-* Hi。宝贝！
-* 这么久了。还没和宝贝说过我的工作呢！
-* 我是个前端工程师。俗称程序员。网页相关。
-* 如这个页面。就是个什么也没有的网页。
-* 我的工作就是给这种空白的页面加点儿东西。
+* Hi。宝！
+* 我们的第一个情人节，希望给你一份特别的礼物。
+*
+* 聊了那么多，但程序员这个身份却太淡了！
+* hh，这才是我的本职工作啊。
+* 让我用代码，来为你写一封信，让网站留存我们的记忆^_^
 * 嗯。说起来手机和电脑还得区分一下。
 * 你现在用的是。。。${isPc ? '电脑' : '手机'}
 */
@@ -106,11 +126,11 @@ html{
   overflow: auto;
   font-size: 14px;
   line-height:1.5;
+  background-size: cover;
   ${ isPc ? '-webkit-transform: rotateY(-10deg) translateZ(-100px) ;transform: rotateY(-10deg) translateZ(-100px) ;' : '-webkit-transform: rotateX(10deg) translateZ(-100px) ;transform: rotateX(10deg) translateZ(-100px) ;' }
     ${ isPc ? '' : '-webkit-transform-origin: 50% 0% 0;transform-origin: 50% 0% 0;' }
   }
-/* 我开始写了 */
-
+/* 我开始写了，看不清没事，一会我调整背景 */
 
 `,
                 `
@@ -118,10 +138,14 @@ html{
  * 因为这是 Markdown 格式的
  * 一种程序员用来写文档日志的简易语言
  * 翻译成 网页 就行了
+ * T T这个背景对眼睛不好，换成纯色
  */
 `,
                 `
 /* 再加点样式 */
+.resumeEditor {
+  background: rgba(255,221,221,.9);
+}
 .resumeEditor{
   padding: 2em;
   line-height:1.8;
@@ -131,21 +155,6 @@ html{
   border-bottom: 1px solid;
   margin: 1em 0 .5em;
   font-size:18px;
-}
-.resumeEditor ul,.resumeEditor ol{
-  list-style: none;
-}
-.resumeEditor ul> li::before{
-  content: '•';
-  margin-right: .5em;
-}
-.resumeEditor ol {
-  counter-reset: section;
-}
-.resumeEditor ol li::before {
-  counter-increment: section;            
-  content: counters(section, ".") " ";  
-  margin-right: .5em;
 }
 .resumeEditor blockquote {
   margin: 1em;
@@ -158,86 +167,54 @@ html{
 `
             ],
             currentMarkdown: '',
-            fullMarkdown: `nostar × juanjuan
+            fullMarkdown: `mxt × anran
 ----
 
-2016年08月05日。初初见面。两人齐齐心动。  
-2016年09月15日。即中秋节阴历八月十五。在一起。  
-已有 \`${getDateDiff((new Date()).getFullYear()+'-'+((new Date()).getMonth()+1)+'-'+(new Date()).getDate(),'2016-09-15') + 1}\` 天
+2025-01-31 23:07:00，是你微信回复我的第一句话。  
+in the moment，我们：  
+\`相识 ${secondsDiff.totalDays} 天 ${secondsDiff.totalHours} 小时 ${secondsDiff.totalMinutes} 分钟 ${secondsDiff.totalSeconds} 秒\`  
 
-一起呲过的餐厅
+相似家庭经历，相同的过年烦恼，不同的成长路线，却还是获得高度同频的思想和审美品位。  
+物理上时间不长，精神上却感觉相识了无数个日夜。  
+以往的时间里，听着相同音乐的我们，不同的时间，体会着相同的感动。  
+
+## 1月31，第一次相识。  
+
+## 2月03，第一次相见，一起唱歌。  
+我们音乐品味是如此的高度一致，从没在任何人身上发生过，让我惊喜。  
+晚饭的畅谈，我从你的眼神，你的回应，看到了共鸣。  
+
+## 2月10，第一次牵你的手，一起逛艺术展。  
+展会上，你说我什么都要点评两句，哈哈哈，行，好可爱。  
+你说我审美不错，可以尝试练字，好开心收获王老师的认可。 
+一起拿到了字帖，一起拍照，一起看了不知道在讲什么的视频，感觉好幸福。  
+地铁上，想要拉住你的手，脑海却一片空白，我感到言语上的匮乏与笨拙。软软的，很小、纤细、美好，转瞬即逝。  
+好快，不想结束，我想跟你下车。  
+
+## 2月11日，第一次亲到你，一起看我的爱情观。  
+这是我仅存的天真与烂漫，没有嘲笑或迎合，我看到你的思考与感受。  
+剧情已经开始发散，一切变得朦胧与迷幻。在你耳边感受你的发香，脑海里飘荡起了王杰的:我是真的爱上你。  
+我强装镇定，手汗出卖了我。  
+片尾曲响起，放空，我感到精神上的交融，大脑告诉我，你的每一个点，终将变为我喜欢的优点，自我洗脑开始了。  
+我变得笨拙，看着你，看窗外，悄悄变红的夜。  
+
+## 暂别
+瞬间，分隔两地，我感到无所适从。我尽力分享我的生活，为了精神上和你在一起，我也想了解你的生活。  
+分享，使我们心的距离缩短。  
+我开始患得患失。我更怕你了解我后，慢慢感到无趣，慢慢厌烦。  
+仅仅是一天的分别，我感到不太对劲，一切都太快了，我无法集中注意力做想你之外的事。  
+这样不像我，太冲动，缺少克制。吃下褪黑素，慢下来，mxt。  
+醒来依旧，去跑步吧，但麻木的大脑没有变化。  
+这时耳机里慵懒的旋律响起：呼吸决定，哈哈，怎么像是点我：“怎么那么坏，才两天就深爱，说的好像真爱”。  
+我释怀了，让情绪决定，跟随呼吸心跳，快慢不由我的理性。  
+
 ----
 
-* 昂吉拉姆西藏餐厅
-* 漫族餐厅
-* 鑫海汇海鲜烧烤
-* 老长沙罐子楼
-* 锅大侠火锅
-* 西湖春天
-* 天意
-* 曼玉
-* 王婆爱上虾
-* 十里洋场
-* 城墙根
-* 爱芳爱德
-* 阳光小店
-* 夜小红虾尾
-* 57°湘
-* 名厨味道
-* 老成都串串
-* 菜捕头
-* 知味观
-* 花港海航度假酒店
-* 冰城烧烤
-* ……
+“把丑事都说了出来，才会听我表白吧，我是真的喜欢你，真的，想谈恋爱了”，和我在一起吧。  
 
-一起看过的电影
-----
 
-1. 七月与安生
-2. 从你的全世界路过
-3. 驴得水
-4. 深海浩劫
-5. 湄公河行动
-6. 你的名字。
-7. 速度与激情8
-8. 战狼2
-9. 敦刻尔克
-10. 正义联盟
-11. 极盗车神
-12. ……
-13. 流浪地球
-14. 飞驰人生
-15. 新喜剧之王
-16. 来电狂响
-17. 疯狂的外星人
-18. 熊出没之原始时代
-19. ……
-
-一起玩过的地方
-----
-
-* 登封少林寺
-* 洛阳
-* 圣王坪
-* 大临淇
-* 皇城相府
-* 杭州西湖
-* ……
-
-一起玩过的游戏
-----
-
-1. 炉石传说
-2. 塞尔达传说-荒野之息
-3. 魂斗罗归来
-4. 马里奥奥德赛
-5. 欢乐麻将
-6. overcooked
-7. ……
-
-> 【Screw the world×I have my dear Juanjuan】  
-> 喂。我不只想影响你的习惯。我还要去改变你的人生。！
+>【理解是最大的沟壑，但我拥有你，anran】  
+>我来陪你走过余下岁月，共同经营，不离不弃。
 
 `
         }
